c1ce4bcf026df5c3319c66a42de73658fb0d9eaa
oskar123456 <cph-oh82@cphbusiness.dk>
2025-04-02 10:58:46 +0200

    .

--- Test/src/main/java/cphdatadvprg/Main.java
+++ Test/src/main/java/cphdatadvprg/Main.java
@@ -12,6 +12,7 @@ import java.awt.event.*;
 import java.awt.geom.*;
 import java.awt.geom.Rectangle2D.Float;
 import java.awt.Graphics2D.*;
+import com.github.javafaker.Faker;
 
 /*
  *
@@ -26,8 +27,119 @@ public class Main
         MainFrame frame = new MainFrame("HelloWorldSwing");
     }
 
+    public static void swap(int[] a, int i, int j)
+    {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void heapSort(int[] a)
+    {
+        int start = a.length / 2;
+        int end = a.length;
+        while (end > 1) {
+            if (start > 0) {
+                --start;
+            } else {
+                --end;
+                swap(a, 0, end);
+            }
+            int root = start;
+            while (2 * root + 1 < end) {
+                int child = 2 * root + 1;
+                if (child + 1 < end && a[child] > a[child + 1]) {
+                    child = child + 1;
+                }
+                if (a[root] > a[child]) {
+                    swap(a, root, child);
+                    root = child;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static <T> void printArrayGeneric(T[] a)
+    {
+        int len = a.length;
+        System.out.printf("[");
+        for (int i = 0; i < len; i++) {
+            System.out.print(a[i]);
+            if (i < len - 1) {
+                System.out.printf(", ");
+            }
+        }
+        System.out.printf("]");
+    }
+
+    public static void printArray(Integer[] a)
+    {
+        int len = a.length;
+        System.out.printf("[");
+        for (int i = 0; i < len; i++) {
+            System.out.printf("%d", a[i]);
+            if (i < len - 1) {
+                System.out.printf(", ");
+            }
+        }
+        System.out.printf("]");
+    }
+
+    public static Integer[] randomArray(int n)
+    {
+        Random rng = new Random(System.nanoTime());
+        int len = Math.abs((rng.nextInt()) % n) + 1;
+        Integer[] a = new Integer[len];
+        for (int i = 0; i < len; i++) {
+            a[i] = rng.nextInt() % 100;
+        }
+        return a;
+    }
+
     public static void main(String[] args)
     {
+
+        int trials = 100;
+        for (int i = 0; i < trials; i++) {
+            Integer[] a = randomArray(25);
+            System.out.println();
+            printArrayGeneric(a);
+            // System.out.printf("%nheapSort:%n", args);
+            StepSorts.heapSortGeneric(a);
+            if (!StepSorts.verifySorted(a)) {
+                System.out.printf("PANICK NOT SORTED");
+            } else {
+                System.out.printf("  --> ");
+            }
+            // StepSorts.heapify(a);
+            // StepSorts.verifyHeap(a);
+            // heapSort(a);
+            printArrayGeneric(a);
+            System.out.println();
+        }
+
+        Faker f = new Faker();
+        for (int i = 0; i < trials; i++) {
+            int len = Math.abs(new Random(System.nanoTime()).nextInt()) % 25 + 1;
+            String[] strs = new String[len];
+            for (int j = 0; j < len; j++) {
+                strs[j] = f.name().firstName();
+            }
+            printArrayGeneric(strs);
+            StepSorts.heapSortGenericCmpr(strs, Comparator.naturalOrder());
+            System.out.printf("  --> ");
+            printArrayGeneric(strs);
+            System.out.println();
+
+            for (int j = 0; j < len - 1; j++) {
+                if (strs[j].compareTo(strs[j + 1]) < 0) {
+                    System.out.printf("PANICK NOT SORTED");
+                }
+            }
+        }
+
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
             public void run() {
                 createAndShowGUI();
--- Test/src/main/java/cphdatadvprg/StepSorts.java
+++ Test/src/main/java/cphdatadvprg/StepSorts.java
@@ -21,5 +21,171 @@ import java.awt.Graphics2D.*;
 
 public class StepSorts
 {
+    public static <T> void heapSortGenericCmpr(T[] a, Comparator<T> cmpr)
+    {
+        int len = a.length;
+        int start = len / 2;
+        int end = len;
+        while (end > 1) {
+            heapifyGenericCmpr(a, end, cmpr);
+            --end;
+            swapGeneric(a, 0, end);
+        }
+    }
 
+    public static <T extends Comparable<T>> void heapSortGeneric(T[] a)
+    {
+        int len = a.length;
+        int start = len / 2;
+        int end = len;
+        while (end > 1) {
+            heapifyGeneric(a, end);
+            --end;
+            swapGeneric(a, 0, end);
+        }
+    }
+
+    public static <T> void swapGeneric(T[] a, int i, int j)
+    {
+        T t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void swap(Integer[] a, int i, int j)
+    {
+        int t = a[i];
+        a[i] = a[j];
+        a[j] = t;
+    }
+
+    public static void heapSort(Integer[] a)
+    {
+        int end = a.length;
+        int start = end / 2;
+        while (end > 1) {
+            if (start > 0) {
+                --start;
+            } else{
+                --end;
+                swap(a, end, 0);
+            }
+            int root = start;
+            while (root * 2 + 1 < end) {
+                int child = root * 2 + 1;
+                if (child + 1 < end && a[child] > a[child + 1]) {
+                    ++child;
+                }
+                if (a[root] > a[child]) {
+                    swap(a, root, child);
+                    root = child;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void heapSort2(Integer[] a)
+    {
+        int len = a.length;
+        int start = len / 2;
+        int end = len;
+        while (end > 1) {
+            heapify(a, end);
+            --end;
+            swap(a, 0, end);
+        }
+    }
+
+    public static boolean verifySorted(Integer[] a)
+    {
+        if (a.length < 2) {
+            return true;
+        }
+        for (int i = 0; i < a.length - 1; i++) {
+            if (a[i] < a[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean verifyHeap(Integer[] a)
+    {
+        int len = a.length;
+        for (int i = 0; i < len; i++) {
+            int l = i * 2 + 1;
+            int r = i * 2 + 2;
+            if (l < len && a[i] >= a[l]) {
+                System.out.printf("not heap: a[%d] = %d <  a[%d] = %d is false%n", i, a[i], l, a[l]);
+                return false;
+            }
+            if (r < len && a[i] >= a[r]) {
+                System.out.printf("not heap: a[%d] = %d <  a[%d] = %d is false%n", i, a[i], r, a[r]);
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static <T> void heapifyGenericCmpr(T[] a, int end, Comparator<T> cmpr)
+    {
+        int start = end / 2;
+        while (start-- > 0) {
+            int root = start;
+            while (root * 2 + 1 < end) {
+                int child = root * 2 + 1;
+                if (child + 1 < end && cmpr.compare(a[child], a[child + 1]) > 0) {
+                    ++child;
+                }
+                if (cmpr.compare(a[root], a[child]) > 0) {
+                    swapGeneric(a, root, child);
+                    root = child;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static <T extends Comparable<T>> void heapifyGeneric(T[] a, int end)
+    {
+        int start = end / 2;
+        while (start-- > 0) {
+            int root = start;
+            while (root * 2 + 1 < end) {
+                int child = root * 2 + 1;
+                if (child + 1 < end && a[child].compareTo(a[child + 1]) > 0) {
+                    ++child;
+                }
+                if (a[root].compareTo(a[child]) > 0) {
+                    swapGeneric(a, root, child);
+                    root = child;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+
+    public static void heapify(Integer[] a, int end)
+    {
+        int start = end / 2;
+        while (start-- > 0) {
+            int root = start;
+            while (root * 2 + 1 < end) {
+                int child = root * 2 + 1;
+                if (child + 1 < end && a[child] > a[child + 1]) {
+                    ++child;
+                }
+                if (a[root] > a[child]) {
+                    swap(a, root, child);
+                    root = child;
+                } else {
+                    break;
+                }
+            }
+        }
+    }
 }
