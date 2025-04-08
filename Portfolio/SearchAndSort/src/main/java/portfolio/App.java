be078a26375e7a3c583d065c67b51111d007a24e
oskar123456 <cph-oh82@cphbusiness.dk>
2025-04-08 07:50:08 +0200

    portfolia

---
+++ Portfolio/SearchAndSort/src/main/java/portfolio/App.java
@@ -0,0 +1,19 @@
+package portfolio;
+
+/**
+ * *********
+ * *********
+ * PORTFOLIO
+ * *************************
+ * *************************
+ * [WEEK 1]: SEARCH AND SORT
+ * *************************
+ * *************************
+ */
+public class App
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+}
